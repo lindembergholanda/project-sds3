43ff464246795a80174a2998a23680f86df7a0ca
Lindemberg Holanda <lindemberg.holanda@gmail.com>
2021-05-04 18:27:36 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,32 @@
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">
+          App desenvolvido por{" "}
+          <a href="https://github.com/lindembergholanda" target="_blank" rel="noreferrer">
+            Lindemberg Holanda
+          </a>
+        </p>
+        <p className="text-light">
+          <small>
+            <strong>Semana Spring React</strong>
+            <br />
+            Evento promovido pela escola DevSuperior:{" "}
+            <a
+              href="https://instagram.com/devsuperior.ig"
+              target="_blank"
+              rel="noreferrer"
+            >
+              @devsuperior.ig
+            </a>
+          </small>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
+
+
