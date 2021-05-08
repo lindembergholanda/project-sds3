079b4b03f30515fd780456e3f3b90035f0d30039
Lindemberg Holanda <lindemberg.holanda@gmail.com>
2021-05-08 09:56:16 -0300

    BarChart integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,6 +1,45 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
 import Chart from "react-apexcharts";
+import { SaleSuccess } from "types/sale";
+import { round } from "utils/format";
+import { BASE_URL } from "utils/request";
+
+type SeriesData = {
+  name: string;
+  data: number[];
+}
+
+type ChartData = {
+  labels: {
+    categories: string[];
+  };
+  series: SeriesData[];
+};
+
+function initializeChartData(categories: string[], nameSeries: string, dataSeries: number[]) {
+  let opts = {
+    labels: { categories: categories },
+    series: [{ name: nameSeries, data: dataSeries }],
+  };
+  return opts;
+}
 
 const BarChart = () => {
+
+  const [chartData, setChartData] = useState<ChartData>(initializeChartData([], "", []));
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales/success-by-seller`).then((response) => {
+      const data = response.data as SaleSuccess[];
+      const myLabels = data.map((x) => x.sellerName);
+      const mySeries = data.map((x) => round(100.0 * (x.deals / x.visited), 1));
+
+      setChartData(initializeChartData(myLabels, "% Success", mySeries));
+    });
+  }, []);
+
   const options = {
     plotOptions: {
       bar: {
@@ -9,20 +48,13 @@ const BarChart = () => {
     },
   };
 
-  const mockData = {
-    labels: {
-      categories: ["Anakin", "Barry Allen", "Kal-El", "Logan", "Padmé"],
-    },
-    series: [
-      {
-        name: "% Sucesso",
-        data: [43.6, 67.1, 67.7, 45.6, 71.1],
-      },
-    ],
-  };
-
   return (
-    <Chart options={{ ...options, xaxis: mockData.labels }} series={mockData.series} type="bar" height="240" />
+    <Chart
+      options={{ ...options, xaxis: chartData.labels }}
+      series={chartData.series}
+      type="bar"
+      height="240"
+    />
   );
 }
 
